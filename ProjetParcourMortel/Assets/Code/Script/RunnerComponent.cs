e1dd5973a7ee61e9ee41977cade7ad53aa1457cd
cedbosse <bosse.ced14@hotmail.com>
2020-09-30 15:40:53 -0400

    Runner Add

---
+++ ProjetParcourMortel/Assets/Code/Script/RunnerComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerComponent : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 Direction;
+    private Animator animator;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(Vector3.Distance(player.position, transform.position) < 10)//distance bidon
+        {
+            animator.SetBool("isPlayerNear", true);
+            Direction = (player.position - transform.position).normalized;
+            transform.position += Direction * 50 * Time.deltaTime;
+        }
+        else
+            animator.SetBool("isPlayerNear", false);
+
+
+    }
+
+
+}
