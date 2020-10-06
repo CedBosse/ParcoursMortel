3f0358fb0cd46a788a0e6a2c9f3a5684c948851e
Dylann Fontus <58182692+Dylannord@users.noreply.github.com>
2020-10-06 19:30:53 -0400

    Animation et Tir
    Ajout des animations de tir ainsi que la balle lors du tir. Ajout de l'animation de l'épée.

---
+++ ProjetParcourMortel/Assets/Source/Script/MouvementComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MouvementComponent : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("isWalking", true);
+            rigidbody.velocity = transform.forward * 1;
+        }
+            
+            
+    }
+}
