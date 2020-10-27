5bd101f8fcc962c97ae5317582502bd6f0cccdc3
gpigeon09 <63869073+gpigeon09@users.noreply.github.com>
2020-10-27 11:26:36 -0400

    NewProject

---
+++ FPS/ProjetImproved/Assets/Source/Scripts/PlayerScripts/PlayerAxeWooshSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAxeWooshSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip[] wooshSound;
+    // Start is called before the first frame update
+    void PlayWooshSound()
+    {
+        audioSource.clip = wooshSound[Random.Range(0, wooshSound.Length)];
+        audioSource.Play();
+    }
+}
