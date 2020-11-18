495292855f086f2ce36b13e8f09016e00979d7cd
cedbosse <bosse.ced14@hotmail.com>
2020-11-17 19:15:52 -0500

    Revert "BossScene"
    This reverts commit fdfd9d3893ac684b9407896638816f96bf9b47bf.

--- FPS/ProjetImproved/Tutorial Defaults/Source/Scripts/EnnemyScripts/MoveForwardComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoveForwardComponent : MonoBehaviour
-{
-    public float force = 500;
-    void Update() => transform.Translate(Vector3.forward * (force * Time.deltaTime), Space.Self);
-}
