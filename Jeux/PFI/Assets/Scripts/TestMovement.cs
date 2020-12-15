54c7a37a7850241d264c95663dbb01b5f8fc4914
gpigeon09 <63869073+gpigeon09@users.noreply.github.com>
2020-12-15 12:56:08 -0500

    DebutPFI

---
+++ Jeux/PFI/Assets/Scripts/TestMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float jumpForce = 2f;
+    [SerializeField] private Rigidbody rb;
+    private int jumpCounter = 0;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TestInput();
+        Jump();
+    }
+    void TestInput()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + 0.005f, transform.position.y, transform.position.z);
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 90, transform.eulerAngles.z);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - 0.005f, transform.position.y, transform.position.z);
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, -90, transform.eulerAngles.z);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (jumpCounter < 2)
+            {
+                Debug.Log("YO");
+                rb.AddForce(jumpForce * Vector3.up);
+                jumpCounter++;
+            }
+            else
+            {
+                jumpCounter = 0;
+            }
+
+        }
+    }
+
+}
