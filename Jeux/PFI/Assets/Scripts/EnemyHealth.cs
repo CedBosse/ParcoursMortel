4891004ac7db2a86c19a258d8eecdf140ed0f455
Dylann Fontus <58182692+Dylannord@users.noreply.github.com>
2020-12-15 20:20:38 -0500

    Ajout du UI et des collisions ( instable )

---
+++ Jeux/PFI/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health = 200;
+    public Image image;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Punch")
+        {
+            health -= 10;
+            image.fillAmount -= 0.1f;
+        }
+        else if(collision.gameObject.tag == "Kick")
+        {
+            health -= 15;
+            image.fillAmount -= 0.15f;
+        }
+    }
+
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            //SceneManager.LoadScene(1);
+        }
+    }
+}
---
+++ Jeux/PFI/Assets/Scripts/KnockbackColiderScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockbackColiderScript : MonoBehaviour
+{
+    public float knockbackStrength;
+    Rigidbody rb;
+
+    private void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, 1f);
+        if (hits.Length > 0)
+        {
+            rb = hits[0].transform.gameObject.GetComponentInChildren<Rigidbody>();
+            if (rb != null)
+            {
+                if (rb.gameObject.tag == "Enemy")
+                {
+                    rb.AddForce(transform.right * knockbackStrength);
+                }
+            }
+        }
+    }
+}
